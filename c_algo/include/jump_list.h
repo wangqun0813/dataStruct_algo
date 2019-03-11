7ee2e1404dc257ab2651d1ad67092905ccd7d35a
wangqun0813 <github0813@163.com>
2019-03-11 23:38:45 +0800

    Add jump list data struct.

---
+++ c_algo/include/jump_list.h
@@ -0,0 +1,25 @@
+#ifndef _JUMP_LIST_H_
+#define _JUMP_LIST_H_
+
+#define FACTOR 4
+
+typedef struct __jList_node_s{
+	unsigned long 	     id;
+	struct __jList_node_s *prev;
+	struct __jList_node_s *next;
+	struct __jList_node_s *down;
+}jList_node_s;
+
+typedef struct __jList_s{
+	unsigned char	level;
+	unsigned long 	count;
+	jList_node_s	*head;
+}jList_s;
+
+void print_jList(jList_s *list);
+jList_node_s* node_init(unsigned long id);
+void jList_insert_by_sort(jList_s *list, jList_node_s *node);
+jList_s* jList_init(void);
+
+
+#endif 
---
+++ c_algo/test/jump_list_test.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "jump_list.h"
+#include "common.h"
+
+
+int main()
+{
+	jList_s *list = NULL;
+	jList_node_s *node = NULL;
+	unsigned long i;
+
+	list = jList_init();
+//	_CHECK_(list == NULL);
+
+	
+	for (i=0; i<20; i++){
+		node = node_init(rand()%100);
+	//	_CHECK_(node == NULL);
+		jList_insert_by_sort(list, node);
+	}
+	print_jList(list);
+
+	return 0;
+}
+
+
+
